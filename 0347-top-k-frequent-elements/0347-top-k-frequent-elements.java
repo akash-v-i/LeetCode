7705c9de3513b10c3af84cd13e875aeb9ae087d7
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-04-22 22:18:32 +0530

    Time: 19 ms (9.82%), Space: 47.3 MB (91.28%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,34 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer>hs=new HashMap<>();
-        //This creates a frequency map
         for(int x:nums){
-            hs.put(x,hs.getOrDefault(x,0)+1);
-        }
-        List<Integer>[] ls=new List[nums.length+1];
-
-        //for accessing the key
-        for(int key:hs.keySet()){
-            int a=hs.get(key);
-            if(ls[a]==null){
-                ls[a]=new ArrayList<>();
-            }
-            ls[a].add(key);
+            hs.putIfAbsent(x,0);
+            hs.put(x,hs.get(x)+1);
         }
 
-        int res[]=new int[k];
-        int c=0;
-        for(int i=ls.length-1;i>=0 && c<k;i--){
-            if(ls[i]!=null){
-                for(Integer x:ls[i]){
-                    res[c++]=x;
-                }
+        PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)-> hs.get(a)-hs.get(b));
+        for(int x:hs.keySet()){
+            pq.add(x);
+            if(pq.size()>k){
+                pq.poll();
             }
         }
-        return res;
+        int arr[]=new int[k];
+        for(int i=0;i<k;i++){
+            arr[i]=pq.poll();
+        }
+        return arr;
     }
 }
-/*
-        The idea is to put all the elements in the map and map them to its frequency 
-        Then add all the elements with same frequency in a Array of list then access it from the last to get the ans
-        */
