54bef58ef60b8d3798abbf6bf7d8eeb6aa89168d
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-04-25 22:40:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -6,7 +6,6 @@ class Solution {
         while(a<b){
             int min=Math.min(height[a],height[b]);
             max=Math.max(max,Math.abs(a-b)*min);
-
             if(height[a]<height[b]){
                 a++;
             }
@@ -19,21 +18,3 @@ class Solution {
 }
 //[1,8,6,2,5,4,8,3,7]
 // 0 1 2 3 4 5 6 7 8
-
-
-/*
-int a=0;
-        int b=height.length-1;
-        int max=0;
-        while(a<b){
-            int min=Math.min(height[a],height[b]);
-            max=Math.max(max,Math.abs(a-b)*min);
-            if(height[a]<height[b]){
-                a++;
-            }
-            else{
-                b--;
-            }
-        }
-        return max;
-*/
