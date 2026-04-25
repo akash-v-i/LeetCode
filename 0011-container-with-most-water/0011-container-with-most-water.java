a624f7749c2e881160207df9ac7941bed00e7e42
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-04-25 22:40:13 +0530

    Time: 6 ms (5.45%), Space: 77.3 MB (50.48%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,25 +1,39 @@
 class Solution {
-    public int maxArea(int[] arr) {
-        //This follows 2 pointer approch where one is from start and one is frm end
-        int first=0;
-        int last= arr.length-1;
+    public int maxArea(int[] height) {
+        int a=0;
+        int b=height.length-1;
         int max=0;
-        while(first<last){
-            int d=last-first;
-            int min=(arr[first]<arr[last])?arr[first]:arr[last];
-            max=Math.max(d*min,max);
-            if(arr[first]<arr[last]){
-                first++;
+        while(a<b){
+            int min=Math.min(height[a],height[b]);
+            max=Math.max(max,Math.abs(a-b)*min);
+
+            if(height[a]<height[b]){
+                a++;
             }
             else{
-                last--;
+                b--;
             }
         }
         return max;
     }
 }
-// arr=[1,8,6,2,5,4,8,3,7] 
-//      0 1 2 3 4 5 6 7 8 
+//[1,8,6,2,5,4,8,3,7]
+// 0 1 2 3 4 5 6 7 8
 
-// arr=[1,1]
-        
+
+/*
+int a=0;
+        int b=height.length-1;
+        int max=0;
+        while(a<b){
+            int min=Math.min(height[a],height[b]);
+            max=Math.max(max,Math.abs(a-b)*min);
+            if(height[a]<height[b]){
+                a++;
+            }
+            else{
+                b--;
+            }
+        }
+        return max;
+*/
