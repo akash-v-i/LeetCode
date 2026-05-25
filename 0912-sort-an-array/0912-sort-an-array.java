3585c338ca7e4e8ee81ccb6e8539dc2fe77d1c06
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-05-25 13:09:42 +0530

    Time: 28 ms (41.77%), Space: 83.1 MB (58.69%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,12 +1,51 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> a - b);
-        for(int x:nums){
-            pq.add(x);
-        }
-        for(int i=0;i<nums.length;i++){
-            nums[i]=pq.poll();
-        }
+        mergesort(nums,0,nums.length-1);
         return nums;
+    }
+
+    public void mergesort(int arr[],int l,int r){
+        if(l<r){
+            int mid=(l+r)/2;
+            mergesort(arr,l,mid);
+            mergesort(arr,mid+1,r);
+            merge(arr,l,r,mid);
+        }
+    }
+
+    public void merge(int arr[],int l,int r,int mid){
+        int n1=(mid-l)+1;
+        int n2=(r-mid);
+
+        int arr1[]=new int[n1];
+        int arr2[]=new int[n2];
+
+        for(int i=0;i<n1;i++){
+            arr1[i]=arr[l+i];
+        }
+
+        for(int i=0;i<n2;i++){
+            arr2[i]=arr[mid+1+i];
+        }
+
+        int i=0;
+        int j=0;
+        int k=l;
+        while(i<n1 && j<n2){
+            if(arr1[i]<=arr2[j]){
+                arr[k]=arr1[i++];
+            }
+            else{
+                arr[k]=arr2[j++];
+            }
+            k++;
+        }
+        while(i<n1){
+            arr[k++]=arr1[i++];
+        }
+        
+        while(j<n2){
+            arr[k++]=arr2[j++];
+        }
     }
 }
