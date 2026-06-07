c2aa966cafb779bc40cd4b1bea1245f11adc6106
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-06-07 10:49:07 +0530

    Time: 3 ms (73.76%), Space: 52.5 MB (64.08%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -11,32 +11,35 @@ class Solution {
                 c2++;
             }
             else if(c1==0){
-                x1=x;
                 c1=1;
+                x1=x;
             }
             else if(c2==0){
-                x2=x;
                 c2=1;
+                x2=x;
             }
             else{
                 c1--;
                 c2--;
             }
         }
-
         c1=0;
-        c2=0;
-        for(int x:nums){
-            if(x1==x) c1++;
-            if(x2==x) c2++;
-        }
-        List<Integer>li=new ArrayList<>();
-        if(c1>nums.length/3){
-            li.add(x1);
-        }
-        if(x1!=x2 && c2>nums.length/3){
-            li.add(x2);
-        }
+            c2=0;
+            for(int a:nums){
+                if(x1==a){
+                    c1++;
+                }
+                if(x1!=x2 && x2==a){
+                    c2++;
+                }
+            }
+            List<Integer>li=new ArrayList<>();
+            if(c1>(nums.length/3)){
+                li.add(x1);
+            }
+            if(c2>(nums.length/3)){
+                li.add(x2);
+            }
         return li;
     }
 }
