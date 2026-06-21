81b27ab5587687327bceb476f3273282d00c82a5
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-06-21 08:07:33 +0530

    Time: 3 ms (82.91%), Space: 46.4 MB (23.57%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,23 +1,28 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> al) {
-        List<List<Integer>>dp=new ArrayList<>();
-        int n=al.size();
-        for(int i=0;i<n;i++){
-            List<Integer>li=new ArrayList<>();
-            for(int j=0;j<=i;j++){
+        int n = al.size();
+        List<List<Integer>> dp = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> li = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
                 li.add(Integer.MAX_VALUE);
             }
             dp.add(li);
         }
-        return find(al,0,n,0,dp);
-    }
-    public static int find(List<List<Integer>>al,int row,int n,int index,List<List<Integer>>dp){
-        if (row==n-1) return al.get(row).get(index);
-        
-        if(dp.get(row).get(index)!=Integer.MAX_VALUE) return dp.get(row).get(index);
 
-        int min=al.get(row).get(index)+Math.min(find(al,row+1,n,index,dp),find(al,row+1,n,index+1,dp));
+        return find(al, dp, 0, 0, n);
+    }
+
+    public int find(List<List<Integer>> al, List<List<Integer>> dp, int row, int index, int n) {
+        if (row == n - 1)
+            return al.get(row).get(index);
+
+        if (dp.get(row).get(index) != Integer.MAX_VALUE)
+            return dp.get(row).get(index);
+        int min =al.get(row).get(index)+ Math.min(find(al, dp, row + 1, index, n),find(al, dp, row + 1, index + 1, n));
         dp.get(row).set(index,min);
+
         return min;
     }
 }
