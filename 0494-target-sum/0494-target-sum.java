2c7663cb10e443f21b13aa4823cf05ea634ef3f6
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-06-22 13:25:52 +0530

    Time: 692 ms (5.04%), Space: 42.9 MB (93.34%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,13 @@
 class Solution {
-    public int findTargetSumWays(int[] arr, int x) {
-        int n=arr.length;
-        HashMap<String,Integer>dp=new HashMap<>();
-        return find(arr,0,x,0,n,dp);
+    public int findTargetSumWays(int[] nums, int target) {
+        return find(nums,target,0,0);
     }
-    public int find(int arr[], int index, int target, int sum, int n, HashMap<String,Integer>dp){
+    public int find(int nums[],int target,int index,int sum){
+        if(index==nums.length && target==sum) return 1;
 
-        if (sum==target && index==n) return 1;
-
-        if (index>=n) return 0;
-
-        if(dp.containsKey(index+"#"+sum)){
-            return dp.get(index+"#"+sum);
-        }
-
-        int positive=find(arr,index+1,target,sum+arr[index],n,dp);
-        int negative=find(arr,index+1,target,sum-arr[index],n,dp);
-        dp.put(index+"#"+sum,positive+negative);
+        if(index>=nums.length) return 0;
+        int positive=find(nums,target,index+1,sum+nums[index]);
+        int negative=find(nums,target,index+1,sum-nums[index]);
         return positive+negative;
     }
 }
