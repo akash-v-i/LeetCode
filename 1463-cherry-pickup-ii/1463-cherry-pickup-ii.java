00009f7d983065551529e447173d54f2552a17c2
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-06-22 13:22:17 +0530

    Time: 26 ms (56.82%), Space: 46.2 MB (75.42%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,7 +1,7 @@
 class Solution {
-    public int cherryPickup(int[][] arr) {
-        int n=arr.length;
-        int m=arr[0].length;
+    public int cherryPickup(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
         int dp[][][]=new int[n][m][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -10,12 +10,12 @@ class Solution {
                 }
             }
         }
-        return find(arr,0,0,m-1,n,m,dp);
+        return find(grid,0,0,m-1,n,m,dp);
     }
+
     public int find(int arr[][],int row,int col1,int col2,int n,int m,int dp[][][]){
-
-        if(col1<0 || col2<0 || col1>=m || col2>=m) return (int)-1e8;
-
+        
+        if(col1<0 || col2<0 || col1>=m || col2>=m) return 0;
         if(row==n-1){
             if(col1==col2){
                 return arr[row][col1];
@@ -25,13 +25,13 @@ class Solution {
 
         if(dp[row][col1][col2]!=-1) return dp[row][col1][col2];
         int max=0;
-        for(int i=-1;i<=1;i++){
-            for(int j=-1;j<=1;j++){
+        for(int i=-1;i<2;i++){
+            for(int j=-1;j<2;j++){
                 if(col1==col2){
-                    max=Math.max(max,arr[row][col1]+find(arr,row+1,col1+i,col2+j,n,m,dp));  
+                    max=Math.max(max,arr[row][col1]+find(arr,row+1,col1+i,col2+j,n,m,dp));
                 }
                 else{
-                    max=Math.max(max,arr[row][col2]+arr[row][col1]+find(arr,row+1,col1+i,col2+j,n,m,dp));
+                    max=Math.max(max,arr[row][col1]+arr[row][col2]+find(arr,row+1,col1+i,col2+j,n,m,dp));
                 }
             }
         }
