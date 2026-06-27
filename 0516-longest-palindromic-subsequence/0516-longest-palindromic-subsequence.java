aba3f25ce03426d59cbb9d604fcbb98154d1ae5e
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-06-27 13:10:21 +0530

    Time: 67 ms (8.52%), Space: 63.6 MB (63.49%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,31 +1,21 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
         StringBuilder st = new StringBuilder(s);
-        String rev = st.reverse().toString();
-
-        int a = s.length();
-        int b = a;
-        int dp[][] = new int[a][b];
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) {
-                dp[i][j] = -1;
+        String rev=st.reverse().toString();
+        int a=s.length();
+        int dp[][]=new int[a+1][a+1];
+        int max=0;
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=a;j++){
+                if(s.charAt(i-1)==rev.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    max=Math.max(max,dp[i][j]);
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-        return find(s, rev, a - 1, b - 1, dp);
-    }
-
-    public int find(String a, String b, int x, int y, int dp[][]) {
-        if (x < 0 || y < 0) {
-            return 0;
-        }
-
-        if (dp[x][y] != -1)
-            return dp[x][y];
-        if (a.charAt(x) == b.charAt(y)) {
-            dp[x][y] = 1 + find(a, b, x - 1, y - 1, dp);
-            return dp[x][y];
-        }
-        dp[x][y] = Math.max(find(a, b, x, y - 1, dp), find(a, b, x - 1, y, dp));
-        return dp[x][y];
+        return max;
     }
 }
