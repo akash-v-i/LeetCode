374c0f0d3fd9ea45d52b5ac9d3b35403fb78767a
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-06-27 16:57:33 +0530

    Time: 25 ms (27.02%), Space: 50.1 MB (96.74%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String shortestCommonSupersequence(String a, String b) {
+        int n=a.length();
+        int m=b.length();
+
+        int dp[][]=new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        return find(dp,n,m,a,b);
+    }
+
+    public String find(int dp[][],int x,int y,String a, String b){
+        if(x==0 || y==0){
+            if(x==0){
+                return b.substring(0,y);
+            }
+            if(y==0){
+                return a.substring(0,x);
+            }
+            return "";
+        }
+
+        if(a.charAt(x-1)==b.charAt(y-1)){
+            return find(dp,x-1,y-1,a,b)+a.charAt(x-1);
+        }
+        else if(dp[x-1][y]>=dp[x][y-1]){
+            return find(dp,x-1,y,a,b)+a.charAt(x-1);
+        }
+        else{
+            return find(dp,x,y-1,a,b)+b.charAt(y-1);
+        }
+    }
+}
