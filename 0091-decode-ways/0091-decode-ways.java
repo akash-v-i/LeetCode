a9198494754c30e1f237009c1a442ea89ed033b9
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-07-05 11:25:38 +0530

    Time: 0 ms (100%), Space: 42.7 MB (96.82%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0')
+            return 0;
+
+        int dp[]=new int[s.length()];
+        Arrays.fill(dp,-1);
+        return find(s, 0,dp);
+    }
+
+    public int find(String str, int index,int dp[]) {
+        if(index==str.length()) return 1;
+
+        if(str.charAt(index)=='0') return 0;
+
+        if(dp[index]!=-1) return dp[index];
+
+        dp[index]=find(str,index+1,dp);
+
+        if(index+1<str.length()){
+            int a=str.charAt(index)-'0';
+            int b=str.charAt(index+1)-'0';
+            int sum=(a*10)+b;
+            if(sum>=10 && sum<=26){
+                dp[index]+=find(str,index+2,dp);
+            }
+        }
+
+        return dp[index];
+        
+    }
+}
+
+/*
+
+         226
+        2 22 26
+
+*/
