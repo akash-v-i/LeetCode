10335791b15a1257cb9f59d9ebddc9a858aae663
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-08-05 12:01:05 +0530

    Time: 1 ms (87.16%), Space: 45.4 MB (63.17%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,32 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        return per(nums);
-    }
-    public List<List<Integer>>per(int nums[]){
+        
+        boolean vis[]=new boolean[nums.length];
         List<List<Integer>>al=new ArrayList<>();
-        back(al,new ArrayList<>(),nums);
-        return al;
+        find(nums,al,new ArrayList<>(),vis);
+        return al;  
     }
-    public void back(List<List<Integer>>al,List<Integer>li,int arr[]){
+
+    public void find(int arr[],List<List<Integer>>al,List<Integer>li,boolean vis[]){
+
         if(li.size()==arr.length){
             al.add(new ArrayList<>(li));
             return;
         }
+
         for(int i=0;i<arr.length;i++){
-            if(li.contains(arr[i])){
+
+            if(i>0 && arr[i]==arr[i-1] && !vis[i]){
                 continue;
             }
+            if(vis[i]) continue;
+
             li.add(arr[i]);
-            back(al,li,arr);
-            li.remove(li.size()-1);
+            vis[i]=true;
+            find(arr,al,li,vis);
+
+            vis[i]=false;
+            li.removeLast();
         }
     }
 }
