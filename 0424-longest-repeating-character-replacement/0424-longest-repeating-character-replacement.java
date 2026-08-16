976ce2f6e2c793b27ef06df93cba536dcff8d697
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-08-16 18:30:33 +0530

    Time: 41 ms (9.97%), Space: 47 MB (24.25%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,29 +1,30 @@
-class Solution {
-    public int characterReplacement(String s, int k) {
-        int max=0;
-        int maxfre=0;
-        int left=0;
-        HashMap<Character,Integer>hs=new HashMap<>();
-        for(int right=0;right<s.length();right++){
-            char ch=s.charAt(right);
-            hs.put(ch,hs.getOrDefault(ch,0)+1);
-            maxfre=Math.max(maxfre,hs.get(ch));
-            int len=right-left+1;
-            if((len-maxfre)<=k){
-                max=Math.max(len,max);
-            }
-            else{
-                while(left<=right && len-maxfre>k){
-                    char a=s.charAt(left++);
-                    hs.put(a,hs.get(a)-1);
-                    maxfre=0;
-                    for(int x:hs.values()){
-                        maxfre=Math.max(maxfre,x);
+    class Solution {
+        public int characterReplacement(String s, int k) {
+            int max=0;
+            int maxfre=0;
+            int left=0;
+            HashMap<Character,Integer>hs=new HashMap<>();
+            for(int right=0;right<s.length();right++){
+                char ch=s.charAt(right);
+                hs.put(ch,hs.getOrDefault(ch,0)+1);
+                maxfre=Math.max(maxfre,hs.get(ch));
+                int len=right-left+1;
+                if((len-maxfre)<=k){
+                    max=Math.max(len,max);
+                }
+                else{
+                    while(left<=right && len-maxfre>k){
+                        char a=s.charAt(left++);
+                        hs.put(a,hs.get(a)-1);
+                        maxfre=0;
+                        for(int x:hs.values()){
+                            maxfre=Math.max(maxfre,x);
+                        }
+                        len=right-left+1;
                     }
-                    len=left-right+1;
+                    max = Math.max(max, len);
                 }
             }
+            return max;
         }
-        return max;
     }
-}
