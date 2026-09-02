adf53341f84d66fddd98ce32426407c117be7028
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-09-02 23:18:24 +0530

    Time: 20 ms (37.02%), Space: 46.2 MB (41.82%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,45 +1,43 @@
 class Solution {
     public String longestPalindrome(String s) {
-
-        if(s.length()<=1){
-            return s;
-        }
-        //take a char and move towards its left and right unit both left and right are same
         String ans="";
-        //there are 2 cases odd and even
-        for(int i=1;i<s.length();i++){
-            int st=i;
-            int end=i;
-            //for odd
-            while(s.charAt(st)==s.charAt(end)){
-                st--;
-                end++;
-                if(st==-1 || end==s.length()){
-                    break;
-                }
+        int max=0;
+        for(int i=0;i<s.length();i++){
+            //odd
+            int left=i-1;
+            int right=i+1;
+            while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+                left--;
+                right++;
             }
-            String pal=s.substring(st+1,end);
-            if(ans.length()<pal.length()){
-                ans=pal;
+            if((right-left +1)>max){
+                max=right-left +1;
+                ans=s.substring(left+1,right);
             }
 
-
-            //for even
-            st=i-1;
-            end=i;
-            while(s.charAt(st)==s.charAt(end)){
-                st--;
-                end++;
-
-                if(st==-1 || end==s.length()){
-                    break;
-                }
+            //even
+            left=i;
+            right=i+1;
+            while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+                left--;
+                right++;
             }
-            pal=s.substring(st+1,end);
-            if(ans.length()<pal.length()){
-                ans=pal;
+            if((right-left +1)>max){
+                max=right-left+1;
+                ans=s.substring(left+1,right);
             }
         }
+        System.out.print(max);
         return ans;
     }
+
 }
+
+//b a b a d
+//0 1 2 3 4
+
+//c b b d
+//0 1 2 3
+
+//b b
+//0 1
