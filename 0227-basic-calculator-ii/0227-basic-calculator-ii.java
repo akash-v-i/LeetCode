e5a520b390b757104de1bb7d6be62b31141420cd
Akash V I <144882052+akash-v-i@users.noreply.github.com>
2026-09-07 13:00:54 +0530

    Time: 47 ms (5.24%), Space: 49.8 MB (6.08%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,50 +1,47 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer>st=new Stack<>();
-        Stack<Character>ch=new Stack<>();
-        int index=0;
-        while(index<s.length()){
-            if(s.charAt(index)==' '){
+        Stack<Integer> st = new Stack<>();
+        Stack<Character> ch = new Stack<>();
+        int index = 0;
+        while (index < s.length()) {
+            if (s.charAt(index) == ' ') {
                 index++;
                 continue;
             }
 
-            if(Character.isDigit(s.charAt(index))){
-                int val=0;
-                while(index<s.length() && Character.isDigit(s.charAt(index))){
-                    val=(val*10)+(s.charAt(index)-'0');
+            if (Character.isDigit(s.charAt(index))) {
+                int val = 0;
+                while (index < s.length() && Character.isDigit(s.charAt(index))) {
+                    val = (val * 10) + (s.charAt(index) - '0');
                     index++;
                 }
-                if(!ch.isEmpty() && (ch.peek()=='*' || ch.peek()=='/')){
-                    if(ch.peek()=='*'){
-                        int temp=st.pop();
+                if (!ch.isEmpty() && (ch.peek() == '*' || ch.peek() == '/')) {
+                    if (ch.peek() == '*') {
+                        int temp = st.pop();
                         ch.pop();
-                        st.push(temp*val);
-                    }
-                    else if(ch.peek()=='/'){
-                        int temp=st.pop();
+                        st.push(temp * val);
+                    } else if (ch.peek() == '/') {
+                        int temp = st.pop();
                         ch.pop();
-                        st.push(temp/val);
+                        st.push(temp / val);
                     }
-                    
-                }
-                else{
-                    if(!ch.isEmpty() && ch.peek()=='-'){
-                        val*=-1;
+
+                } else {
+                    if (!ch.isEmpty() && ch.peek() == '-') {
+                        val *= -1;
                     }
                     st.push(val);
                 }
-            }
-            else if(index<s.length() && s.charAt(index)!=' '){
+            } else if (index < s.length() && s.charAt(index) != ' ') {
                 ch.push(s.charAt(index));
                 index++;
             }
         }
-        while(!ch.isEmpty()){
-            int a =st.pop();
-            int b=st.pop();
-            char cha=ch.pop();
-            st.push(a+b);
+        while (!ch.isEmpty()) {
+            int a = st.pop();
+            int b = st.pop();
+            char cha = ch.pop();
+            st.push(a + b);
         }
         return st.pop();
     }
